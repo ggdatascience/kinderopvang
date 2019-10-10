20ec4c691edaac6d6b2979029fb5584908312d53
graskaas2014 <graskaas2014@users.noreply.github.com>
2019-10-10 20:43:04 +0200

    function fb link door jeroen

--- FB_scrapen_viaLRK.R
+++ FB_scrapen_viaLRK.R
@@ -1,80 +1,50 @@
-#https://www.datacamp.com/community/tutorials/r-web-scraping-rvest
-
+#Verkrijgen facebooklinks adv open LRK dataset
+#Chris Elschot & Jeroen ter Voert
+#Laatste update oktober 2019
+
+#factors omzetten naar strings
 options(stringsAsFactors = FALSE)
-
-# General-purpose data wrangling
-require(tidyverse)  
-
-# Parsing of HTML/XML files  
-require(rvest)    
-
-# String manipulation
-require(stringr)   
-
-# Verbose regular expressions
-require(rebus)     
-
-# Eases DateTime manipulation
-require(lubridate)
-# tekst matching
-library(data.table)
-library(caret)
-library(e1071)
-
+
+#vereiste packages laden
+#library(curl)
+library(rvest)
+
+#data via https://data.overheid.nl/dataset/gegevens-kinderopvanglocaties-lrk
+#inlezen data kinderopvang
+df.lrk <- read.csv2("http://www.landelijkregisterkinderopvang.nl/opendata/export_opendata_lrk.csv")
+#gastouders eruit
+df.lrk <- df.lrk[df.lrk$type_oko != "VGO", ]
+#geen website naar NA
+df.lrk$contact_website[which(df.lrk$contact_website == "")] <- NA
+
 #gebruik proxy settings via curl (ivm firewall issues))
-curl_proxy <- function(url, verbose = TRUE){
-  proxy <- ie_get_proxy_for_url(url)
-  h <- new_handle(verbose = verbose, proxy = proxy)
-  curl(url, handle = h)
-}
-
-#inlezen data kinderopvang, excl gastouders (zie google drive voor bestand)
-#datascrape <- read.csv2("<vulje bestandlocatie in")
-datascrape <- filter(export_opendata_lrk, type_oko != 'VGO')
-
-
-#URL friendly format (UTF-8) 
-#url <- str_conv(datascrape$contact_website, "UTF-8")
-url <- as.data.frame(as.character(datascrape$contact_website))
-names(url)[1] <- 'v1'
-#test met 100
-url <- head(url,100)
-
-
-# Afzonderlijke pagina's scrapen
-# url facebook ophalen
-get_url <- function(url){
-  html <- read_html(url)
-  FBurl <- html %>% html_nodes("a") %>% html_attr("href") 
-  #unlist()                    
-  # Alleen de facebook link opslaan
-  FBtable <- data.table(FBurl)
-  FBtable[, test := grepl("facebook.com", FBurl), by = FBurl]
-  FBtable <- subset(FBtable, test == TRUE, FBurl)
-  FBlink <- head(FBtable, 1)
-  return(FBlink)
-  
-} # 
-
-df.fb <- data.frame(id = integer(), url= character(), fburl = character())
-
-for (i in 1:nrow(url)){
-  
-  if (url$v1[i] == '' ){
-    
-    #df.fb[i, 1] <- id # nog ergens ophalen en wegschrijven
-    df.fb[i, 2] <- url$v1[i]
-    df.fb[i, 3] <- 'geen fb link'
-    
-  } else {
-    
-  df.fb[i, 2] <- url$v1[i]
-  df.fb[i, 3] <- get_url(url$v1[i]) #try catch inbouwen, gaat fout in de functie
-  
+#curl_proxy <- function(url, verbose = TRUE){
+#  proxy <- ie_get_proxy_for_url(url)
+#  h <- new_handle(verbose = verbose, proxy = proxy)
+#  curl(url, handle = h)
+#}
+
+#functie die op contact_website zoekt naar facebook websites
+get_FBurl <- function(url){
+  if (is.na(url)){
+    return(list(homepage = url, facebookpage = NA)) #testen of input NA is, zoja return NA
   }
+  try(html <- read_html(url)) #Inlezen html van de webpagina
+  if (exists('html')){ #kijken of object all_urls bestaat
+    all_urls <- html %>%
+      html_nodes("a") %>% #Hyperlinks worden gedefinieerd met de HTML <a> tag
+      html_attr("href") #Het href attribuut bevat het webadres waar naar gelinkt wordt
+    FBurl <- unique(all_urls[which(grepl('facebook.com', all_urls))]) #Alleen de unieke adressen met "facebook.com" erin
+    if (length(FBurl) > 0){
+      return(list(homepage = url, facebookpage = FBurl)) #kan meerdere waarden bevatten dus returnen als list
+    }
+  }
+  return(list(homepage = url, facebookpage = NA)) # Indien er geen valid homepage is of er geen facebook links zijn return NA
 }
-
-results <- NULL
-
-   
-}
+
+#fb urls voor de 1e 20
+#fburls <- lapply(df.lrk$contact_website[1:20], get_FBurl)
+
+#fb urls voor hele bestand. Index is gelijk aan die van het dataframe df.lrk
+#om het script sneller te maken zouden de NA's uit de input gefilterd kunnen worden, deze returnen namelijk toch altijd NA.
+fburls <- lapply(df.lrk$contact_website, get_FBurl)
